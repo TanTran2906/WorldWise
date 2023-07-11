31bae94b1873aec206480a29c4983a114538bfe2
TanTran2906 <86616223+TanTran2906@users.noreply.github.com>
2023-07-12 00:17:06 +0700

    Context

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,30 @@
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import CoutryItem from "./CountryItem";
+
+function CountryList({ cities, isLoading }) {
+    if (isLoading) return <Spinner />;
+
+    if (!cities.length)
+        return (
+            <Message message="Add your first city by clicking on a city on a map" />
+        );
+    //Trả về những thành phố không bị trùng
+    const countries = cities.reduce((arr, city) => {
+        if (!arr.map((el) => el.country).includes(city.country))
+            return [...arr, { country: city.country, emoji: city.emoji }];
+        else return arr;
+    }, []);
+    console.log(countries);
+
+    return (
+        <ul className={styles.countryList}>
+            {countries.map((country) => (
+                <CoutryItem country={country} key={countries.country} />
+            ))}
+        </ul>
+    );
+}
+
+export default CountryList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css"; //File css global: Mọi component có thể sử dụng CSS từ file index.css
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
